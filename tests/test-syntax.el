827985e4df3753e4db43ed61509da604a6cccf8d
Yuta Yamada <cokesboy@gmail.com>
2015-07-06 21:54:45 -0700

    Add a helper function to use `buttercup-run-at-point`
    Inside describe function, you can use `buttercup-run-at-point`.

--- tests/test-syntax.el
+++ tests/test-syntax.el
@@ -10,6 +10,12 @@
  (after-each
   (kill-buffer (get-buffer-create "*Test*")))
 
+ (defun test-concat-dir (filepath)
+   (if noninteractive
+       filepath
+     (defvar-local test-syntax-dir (concat (locate-dominating-file buffer-file-name ".git")))
+     (concat test-syntax-dir filepath)))
+
  (defun test-faces (test-string file-name faces)
    (lexical-let ((file-name file-name) (faces faces))
      (it test-string
@@ -25,6 +31,6 @@
  ;; (text-properties-at pos (get-buffer "file.nim"))
  (test-faces
   "should highlight strings"
-  "tests/syntax/string.nim"
+  (test-concat-dir "tests/syntax/string.nim")
   '((10 . font-lock-string-face)
     (5 . font-lock-variable-name-face))))
