1c8fac5f64cc6bda5bed002e411525255894057d
Yuta Yamada <cokesboy@gmail.com>
2015-08-09 16:35:29 -0700

    Modify uncompleted-indent's default value to 4
    I discussed [here](http://forum.nim-lang.org/t/1507) and Araq
    usually uses 4 spaces as indent.
    
    Conflicts:
    	nim-vars.el

--- nim-vars.el
+++ nim-vars.el
@@ -57,7 +57,7 @@
 
 (defconst nim-indent-offset 2 "Number of spaces per level of indentation.")
 
-(defvar nim-uncompleted-condition-indent 'stmt+1
+(defvar nim-uncompleted-condition-indent 4
   "Indent behavior when condition doesn't end on one line.
 You can specify number or 'stmt+1.")
 
