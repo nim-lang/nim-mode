1a8910af5b56bd47d009c9ec0543b02abc7a0bf0
Yuta Yamada <cokesboy@gmail.com>
2015-08-09 16:06:04 -0700

    Modify test to specify number of indent

--- tests/test-indent.el
+++ tests/test-indent.el
@@ -7,9 +7,11 @@
 
 (describe
  "Indentation"
- (defun indent-and-compare (test-string file-name)
-   (lexical-let ((act (concat file-name "-actual.nim")) (exp (concat file-name "-expected.nim")))
+ (cl-defun indent-and-compare (test-string file-name &key (uncompleted-indent 4))
+   (lexical-let ((act (concat file-name "-actual.nim")) (exp (concat file-name "-expected.nim"))
+                 (indent uncompleted-indent))
      (it test-string
+         (setq nim-uncompleted-condition-indent indent)
          (insert-file-contents-literally act)
          (indent-region (point-min) (point-max))
 
@@ -62,8 +64,10 @@
   "tests/indents/when-stmt")
 
  (indent-and-compare
-  "should indent after uncompleted condition correctly"
-  "tests/indents/uncompleted-condition")
+  "should indent after uncompleted condition correctly by stmt+1 option"
+  "tests/indents/uncompleted-condition"
+  :uncompleted-indent 'stmt+1)
+
  )                                      ; keep this here for less changes
 
 ;; Local Variables:
