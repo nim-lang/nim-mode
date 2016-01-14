89d8d6e6113f026a5ba397b6390f0632a61c86a6
Yuta Yamada <cokesboy@gmail.com>
2016-01-14 08:29:22 -0800

    Move break's test

--- tests/indents/SMIE/block.nim
+++ tests/indents/SMIE/block.nim
@@ -36,3 +36,11 @@ proc testBlock5 =
       echo "something"
       break
     echo "after break should be dedented"
+
+
+proc breakTest() =
+  while true:
+    if size == result.len: break
+    let data = await socket.recv(size - result.len)
+    if data == "": break # We've been disconnected.
+    result.add data
--- tests/indents/SMIE/break.nim
+++
@@ -1,6 +0,0 @@
-proc breakTest() =
-  while true:
-    if size == result.len: break
-    let data = await socket.recv(size - result.len)
-    if data == "": break # We've been disconnected.
-    result.add data
