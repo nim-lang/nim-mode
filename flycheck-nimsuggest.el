1b87c71c39d4373fca7d445e486bdf9c20ea2175
Yuta Yamada <cokesboy@gmail.com>
2017-04-08 21:27:30 -0700

    Fix error line position (hopefully)

--- flycheck-nimsuggest.el
+++ flycheck-nimsuggest.el
@@ -79,8 +79,9 @@ CALLBACK is the status callback passed by Flycheck."
 CHECKER and BUFFER are passed to flycheck's function."
   (cl-loop for e in errors
            for file   = (nth 3 e)
-           for line   = (1+ (nth 5 e))
-           for column = (nth 6 e)
+           for line   = (nth 5 e)
+           ;; column starts from 0 on nimsuggest, but emacs is not
+           for column = (1+ (nth 6 e))
            for msg    = (nth 7 e)
            for level  = (if (equal "Error" (nth 4 e))
                             'error
