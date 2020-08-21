ceaad6448a4f209c3d2cb5bd3aad1aa67555beb3
mcbloch <nim-mode@mcbloch.dev>
2020-08-21 04:32:30 +0200

    Remove unneeded info about author etc.

--- nim-nimble.el
+++ nim-nimble.el
@@ -1,25 +1,8 @@
 ;;; nim-nimble.el --- Providing nimble commands -*- lexical-binding: t; -*-
-;;
-;; Copyright (C) 2020 Maxime Bloch
-;;
-;; Author: Maxime Bloch <http://github/maxime>
-;; Maintainer: Maxime Bloch <me@mcbloch.dev>
-;; Created: August 20, 2020
-;; Modified: August 20, 2020
-;; Version: 0.0.1
-;; Keywords:
-;; Homepage: https://github.com/maxime/nim-nimble
-;; Package-Requires: ((emacs 27.1) (cl-lib "0.5"))
-;;
-;; This file is not part of GNU Emacs.
-;;
+
 ;;; Commentary:
-;;
-;;
-;;
+
 ;;; Code:
-
-
 (require 'nim-vars)
 (require 'nim-compile)
 (require 'ansi-color)
